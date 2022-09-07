8d8d33989573326286f1202851d3b938d2368cc5
tutneshta <tutneshta@yandex.ru>
2022-09-07 13:26:12 +0300

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.IO;
+using System.Runtime.Serialization.Formatters.Binary;
+using System.Threading.Tasks;
+using System.Security.Principal;
+
+namespace FinalTask
+{
+    class Program
+    {
+
+
+        static async Task Main(string[] args)
+        {
+
+            string Filepath = @"C:\Users\s.bury\Desktop\Students.dat";
+
+            CreateStudentOnDesktop(Filepath);
+
+        }
+
+        public static void CreateStudentOnDesktop(string Filepath)
+        {
+            string DirPath = @"C:\Users\s.bury\Desktop\Students\";
+
+            DirectoryInfo directoryInfo = new DirectoryInfo(DirPath);
+
+            if (!directoryInfo.Exists)
+            {
+                directoryInfo.Create();
+            }
+            BinaryFormatter formatter = new BinaryFormatter();
+
+            using (FileStream fs = new FileStream(Filepath, FileMode.OpenOrCreate))
+            {
+                Student[] student = (Student[])formatter.Deserialize(fs);
+
+                for (int i = 0; i < student.Length; i++)
+                {
+                    using (StreamWriter writer = new StreamWriter($"{DirPath}{student[i].Group}.txt", true))
+                    {
+                        writer.WriteLine($"{student[i].Name}, {student[i].DateOfBirth}");
+                    }
+
+                }
+            }
+
+        }
+    }
+
+}
---
+++ Student.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace FinalTask
+{
+    [Serializable]
+    public class Student
+    {
+        public string Name { get; set; }
+        public string Group { get; set; }
+        public DateTime DateOfBirth { get; set; }
+    }
+}
